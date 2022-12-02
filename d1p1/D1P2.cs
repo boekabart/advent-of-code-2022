2191c0592dd59e8363689dd55b7951d36350eaca
Bart de Boer <bart.de.boer@divverence.com>
2022-12-02 08:40:54 +0100

    Solve day 1 puzzle 2

---
+++ d1p1/D1P2.cs
@@ -0,0 +1,32 @@
+﻿namespace d1p1;
+
+internal static class D1P2
+{
+
+    private record Aggregate(int Value = 0, Aggregate? Previous = null);
+
+    public static int GetCaloriesOfHighestElves(IEnumerable<int?> input, int elveCount)
+    {
+        return input
+            .Aggregate(new Aggregate(), DoAggregate).GetAll().OrderByDescending( val => val).Take(elveCount).Sum();
+;
+    }
+
+    private static IEnumerable<int> GetAll(this Aggregate? aggregate)
+    {
+        while (aggregate is not null)
+        {
+            yield return aggregate.Value;
+            aggregate = aggregate.Previous;
+        }
+    }
+
+    private static Aggregate DoAggregate(this Aggregate prev, int? number) =>
+        number is null ? prev.Reset() : prev.Add(number.Value);
+    private static Aggregate Reset(this Aggregate aggregate) => new(0, aggregate);
+    private static Aggregate Add(this Aggregate aggregate, int number)
+    {
+        var newValue = aggregate.Value + number;
+        return new(newValue,  aggregate.Previous);
+    }
+}
---
+++ d1p1/D1P2Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace d1p1;
+
+public class D1P2Tests
+{
+    [Fact]
+    void CountingIsOkFor1()
+    {
+        var input = new int?[] { 1000, 2000, null, 6000, 1000, null, 4000, 2000, 100 };
+        var actual = D1P2.GetCaloriesOfHighestElves(input,1);
+        actual.Should().Be(7000);
+    }
+
+    [Fact]
+    void CountingIsOkFor3()
+    {
+        var input = new int?[] { 1000, 2000, null, 6000, 1000, null, 4000, 2000, 100 };
+        var actual = D1P2.GetCaloriesOfHighestElves(input, 3);
+        actual.Should().Be(16100);
+    }
+}
