d44da9f6aea9a050f90eaea5de8f3acd067811cc
Bart de Boer <bart.de.boer@divverence.com>
2022-12-08 14:53:03 +0100

    Add some useful shared extension methods

---
+++ shared/GridExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿namespace shared;
+
+public static class GridExtensionMethods
+{
+    public static IEnumerable<IEnumerable<TValue>> Columns<TValue>(this TValue[][] grid) => grid.ColumnIndices().Select(grid.Column);
+    public static IEnumerable<IEnumerable<TValue>> Rows<TValue>(this IEnumerable<IEnumerable<TValue>> grid) => grid;
+
+    public static IEnumerable<TValue> Column<TValue>(this TValue[][] grid, int x) => grid.Select(row => row[x]);
+
+    public static IEnumerable<int> ColumnIndices<TValue>(this TValue[][] grid) => Enumerable.Range(0, grid[0].Length);
+
+    public static IEnumerable<TResult> RowSelectMany<TInput, TResult>(this TInput[][] grid,
+        Func<TInput[], int, IEnumerable<TResult>> func) =>
+        grid.SelectMany(func);
+
+    public static IEnumerable<TResult> ColumnSelectMany<TInput, TResult>(this TInput[][] grid,
+        Func<TInput[], int, IEnumerable<TResult>> func) =>
+        grid.ColumnIndices().Select(i => grid.Column(i).ToArray()).SelectMany(func);
+
+    public static IEnumerable<TResult> RowSelectMany<TInput, TResult>(this TInput[][] grid,
+        Func<IEnumerable<TInput>, IEnumerable<TResult>> func) =>
+        grid.Rows().SelectMany(func);
+
+    public static IEnumerable<TResult> ColumnSelectMany<TInput, TResult>(this TInput[][] grid,
+        Func<IEnumerable<TInput>, IEnumerable<TResult>> func) =>
+        grid.Columns().SelectMany(func);
+
+    public static IEnumerable<IEnumerable<TResult>> Select<TValue, TResult>(this IEnumerable<IEnumerable<TValue>> grid,
+        Func<TValue, int, int, TResult> mapWithCoordinates) =>
+        grid.Rows().Select((row, y) => row.Select((item, x) => mapWithCoordinates(item, x, y)));
+
+    public static TValue[][] AsGridArray<TValue>(this IEnumerable<IEnumerable<TValue>> grid) =>
+        grid is TValue[][] gridArray ? gridArray : grid.Rows().Select(row => row.AsArray()).ToArray();
+
+    public static TValue[] AsArray<TValue>(this IEnumerable<TValue> row) =>
+        row is TValue[] array ? array : row.AsArray();
+}
---
+++ shared/MoreLinq.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace shared;
+
+public static class MoreLinq
+{
+    public static IEnumerable<TResult> Scan<TInput, TState, TResult>(this IEnumerable<TInput> src,
+        Func<TState, TInput, int, (TResult, TState)> mapFunc) => src.Scan(default, mapFunc!);
+
+    public static IEnumerable<TResult> Scan<TInput, TState, TResult>(this IEnumerable<TInput> src, TState state,
+        Func<TState, TInput, int, (TResult, TState)> mapFunc)
+    {
+        foreach (var (input, index) in src.WithIndex())
+        {
+            (var result, state) = mapFunc(state, input, index);
+            yield return result;
+        }
+    }
+
+    public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
+        => self.Select((item, index) => (item, index));
+
+    public static IEnumerable<string> TrimmedLines(this string multiLineString) =>
+        multiLineString.Split(new[] {'\n'})
+            .Select(s => s.Trim());
+
+    public static IEnumerable<string> Lines(this string multiLineString) =>
+        multiLineString
+            .Replace("\r\n", "\n")
+            .Split(new[] { '\n' });
+
+    public static IEnumerable<string> NotEmptyLines(this string multiLineString) =>
+        multiLineString
+            .Lines()
+            .Where(s => s.Length > 0);
+
+    public static IEnumerable<string> NotEmptyTrimmedLines(this string multiLineString) =>
+        multiLineString
+            .TrimmedLines()
+            .Where(s => s.Length > 0);
+
+    /// <summary>
+    /// Checks whether an enumerable is completely distinct
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
+    /// <param name="items"></param>
+    /// <returns>true if they are all distinct, false if there are duplicates, true if there are none</returns>
+    public static bool AreDistinct<TItem>(this IEnumerable<TItem> items)
+    {
+        var hashSet = new HashSet<TItem>();
+        return items.All(hashSet.Add);
+    }
+}
