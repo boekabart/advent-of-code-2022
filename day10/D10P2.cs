2fa0a9701acd7eac5f5606cd87a00ffc5c5882fb
Bart de Boer <bart.de.boer@divverence.com>
2022-12-10 07:54:32 +0100

    Finish Day 10 Part 2

--- day10/D10P2.cs
+++ day10/D10P2.cs
@@ -1,7 +1,26 @@
-﻿namespace day10;
+﻿using shared;
+
+namespace day10;
 
 public static class D10P2
 {
-    public static object Part2Answer(this string input) =>
-        new NotImplementedException();
+    public static IEnumerable<string> Part2Answer(this string input) =>
+        input
+            .ParseInstructions()
+            .Expand()
+            .Execute()
+            .ScanLines()
+            .Render();
+
+    internal static IEnumerable<CpuRegisters[]> ScanLines(this IEnumerable<CpuRegisters> src)
+        => src.Buffer(40);
+
+    internal static IEnumerable<string> Render(this IEnumerable<CpuRegisters[]> src)
+        => src.Select(RenderLine);
+
+    internal static string RenderLine(this CpuRegisters[] regs)
+        => new(regs.Select(RenderPixel).ToArray());
+
+    internal static char RenderPixel(this CpuRegisters reg, int scan)
+        => Math.Abs(reg.X - scan) >= 2 ? '.' : '█';
 }
--- day10/D10P2Tests.cs
+++ day10/D10P2Tests.cs
@@ -1,25 +1,65 @@
 ﻿using FluentAssertions;
+using shared;
 using Xunit;
 
 namespace day10;
 
 public class D10P2Tests
 {
-    [Fact(Skip = "ToDo")]
-    internal static void AcceptanceTest()
+    [Fact]
+    internal static void RenderPixelTest()
     {
-        var expected = 42;
-        Input.ExampleInput
-            .Part2Answer()
-            .Should().Be(expected);
+        var line = new[] { 1, 1, 16, 16, 5, 5, 11, 11, 8, 8, 13, 13, 12, 12, 4, 4, 17, 17, 21, 21, 21 };
+        var l = line.Select(a => new CpuRegisters(a)).ToArray();
+
+        new CpuRegisters(13).RenderPixel(11).Should().Be('·');
+        new CpuRegisters(13).RenderPixel(12).Should().Be('█');
+        new CpuRegisters(13).RenderPixel(13).Should().Be('█');
+        new CpuRegisters(13).RenderPixel(14).Should().Be('█');
+        new CpuRegisters(13).RenderPixel(15).Should().Be('·');
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
+    internal static void RenderLineTest()
+    {
+        var line = new[] {1, 1, 16, 16, 5, 5, 11, 11, 8, 8, 13, 13, 12, 12, 4, 4, 17, 17, 21, 21, 21}
+            .Select(a => new CpuRegisters(a)).ToArray();
+
+        var actual = line.RenderLine();
+        actual.Should().Be("██··██··██··██··██··█");
+    }
+
+    [Fact]
+    internal static void AcceptanceTest()
+    {
+        var expectedText = @"
+██··██··██··██··██··██··██··██··██··██··
+███···███···███···███···███···███···███·
+████····████····████····████····████····
+█████·····█████·····█████·····█████·····
+██████······██████······██████······████
+███████·······███████·······███████·····
+";
+        var expected = expectedText.NotEmptyTrimmedLines();
+        Input.ExampleInput
+            .Part2Answer()
+            .Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expectedText = @"
+████·███····██·███··███··█··█··██··█··█·
+█····█··█····█·█··█·█··█·█·█··█··█·█··█·
+███··█··█····█·███··█··█·██···█··█·████·
+█····███·····█·█··█·███··█·█··████·█··█·
+█····█····█··█·█··█·█·█··█·█··█··█·█··█·
+████·█·····██··███··█··█·█··█·█··█·█··█·
+";
+        var expected = expectedText.NotEmptyTrimmedLines();
         Input.RawInput
             .Part2Answer()
-            .Should().Be(expected);
+            .Should().BeEquivalentTo(expected);
     }
 }
