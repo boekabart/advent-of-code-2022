6166b6b82c1d99692f0d4e47428cfb566f11ea60
Bart de Boer <bart.de.boer@divverence.com>
2022-12-10 07:00:22 +0100

    Pass acc. test of Day10 Part 1

--- day10/D10P1.cs
+++ day10/D10P1.cs
@@ -1,23 +1,51 @@
-﻿namespace day10;
+﻿using shared;
 
-internal record Thing(bool Data);
+namespace day10;
+
+internal record Instruction(int Duration, int DeltaX);
+internal record TickAction(int DeltaX);
+
+internal record CpuRegisters(int X = 1);
 
 public static class D10P1
 {
     public static object Part1Answer(this string input) =>
-        new NotImplementedException();
-
-    internal static IEnumerable<Thing> ParseThings(this string input) =>
         input
-            .Split(new[] {'\n'})
-            .Select(TryParseAsThing)
-            .OfType<Thing>();
+            .ParseInstructions()
+            .Expand()
+            .Execute()
+            .GetSignalStrengths()
+            .GetTheSixRequestedValuesForX()
+            .Sum();
 
-    internal static Thing? TryParseAsThing(this string line)
+    internal static IEnumerable<Instruction> ParseInstructions(this string input) =>
+        input
+            .TrimmedLines()
+            .Select(TryParseAsThing)
+            .OfType<Instruction>();
+
+    internal static Instruction? TryParseAsThing(this string line)
     {
+        if (line.Equals("noop")) return new(1, 0);
+        if (line.StartsWith("addx ") && int.TryParse(line[5..], out var deltaX)) return new(2, deltaX);
         return null;
     }
 
-    internal static int GetResult(this IEnumerable<Thing> things) => things.Select(AsResult).Sum();
-    internal static int AsResult(this Thing thing) => 0;
+    internal static IEnumerable<TickAction> Expand(this IEnumerable<Instruction> src) =>
+        src.SelectMany(instruction => Enumerable.Range(0, instruction.Duration).Select(idx => new TickAction(idx * instruction.DeltaX)));
+
+    internal static IEnumerable<CpuRegisters> Execute(this IEnumerable<TickAction> src) =>
+        src.Scan(new CpuRegisters(), (reg, action, _) => (reg, action.Execute(reg)));
+
+    internal static CpuRegisters Execute(this TickAction action, CpuRegisters regs)
+        => new(X: regs.X + action.DeltaX);
+
+    internal static IEnumerable<int> GetSignalStrengths(this IEnumerable<CpuRegisters> src) =>
+        src.Select((reg, idx) => reg.X * (idx + 1));
+
+    internal static IEnumerable<int> GetTheSixRequestedValuesForX(this IEnumerable<int> src) =>
+        src
+            .Buffer(40)
+            .Take(6)
+            .Select(buf => buf[19]);
 }
--- day10/D10P1Tests.cs
+++ day10/D10P1Tests.cs
@@ -5,25 +5,64 @@ namespace day10;
 
 public static class D10P1Tests
 {
-    [InlineData("",null)]
+    [InlineData("noop", 1)]
+    [InlineData("", null)]
+    [InlineData("addx -11", 2, -11)]
+    [InlineData("addx 0",2, 0)]
     [Theory]
-    internal static void ParseInputLineTest(string line, Thing? expectedThing)
+    internal static void ParseInputLineTest(string line, int? duration, int deltaX = 0)
     {
         var actualThing = line.TryParseAsThing();
-        actualThing.Should().Be(expectedThing);
+        if (duration is null)
+        {
+            actualThing.Should().BeNull();
+            return;
+        }
+        actualThing.Should().NotBeNull();
+        actualThing!.Duration.Should().Be(duration);
+        actualThing.DeltaX.Should().Be(deltaX);
     }
 
     [Fact]
     internal static void ParseInputTest()
     {
-        var things = Input.ExampleInput.ParseThings().ToArray();
-        things.Should().HaveCount(0);
+        var things = Input.ExampleInput.ParseInstructions().ToArray();
+        things.Should().HaveCount(146);
     }
 
-    [Fact(Skip="ToDo")]
+    [Fact]
+    internal static void ExpandTest()
+    {
+        var things = Input.ExampleInput.ParseInstructions().Expand().ToArray();
+        things.Should().HaveCount(240);
+        things[0].DeltaX.Should().Be(0);
+        things[1].DeltaX.Should().Be(15);
+        things[2].DeltaX.Should().Be(0);
+        things[3].DeltaX.Should().Be(-11);
+    }
+
+    [Fact]
+    internal static void ExecuteTest()
+    {
+        var things = Input.ExampleInput.ParseInstructions().Expand().Execute().ToArray();
+        things.Should().HaveCount(240);
+        things[19].X.Should().Be(21);
+        things[59].X.Should().Be(19);
+    }
+
+    [Fact]
+    internal static void GetSignalStrengthsTest()
+    {
+        var things = Input.ExampleInput.ParseInstructions().Expand().Execute().GetSignalStrengths().ToArray();
+        things.Should().HaveCount(240);
+        things[19].Should().Be(21 * 20);
+        things[59].Should().Be(19 * 60);
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 13140;
         Input.ExampleInput
             .Part1Answer()
             .Should().Be(expected);
@@ -36,5 +75,12 @@ public static class D10P1Tests
         Input.RawInput
             .Part1Answer()
             .Should().Be(expected);
+    }
+
+    [Fact(Skip = "ToDo")]
+    internal static void RawInputLeadsTo220Signals()
+    {
+        var things = Input.RawInput.ParseInstructions().Expand().Execute().GetSignalStrengths().ToArray();
+        things.Length.Should().BeGreaterOrEqualTo(220);
     }
 }
