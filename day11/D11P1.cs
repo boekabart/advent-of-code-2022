2456023daaa6a840a8d2fc9de324796e09f7cee1
Bart de Boer <bart.de.boer@divverence.com>
2022-12-11 09:24:47 +0100

    Solve Day 11 Part 1

--- day11/D11P1.cs
+++ day11/D11P1.cs
@@ -1,25 +1,126 @@
-﻿using shared;
+﻿using System.Collections.Immutable;
+using shared;
 
 namespace day11;
 
-internal record Thing(bool Data);
+internal record Monkey(int Index, ImmutableQueue<int> Items, Func<int,int> Operation, Func<int,bool> Test, int TrueMonkey, int FalseMonkey);
+
+internal record Game(ImmutableArray<Monkey> Monkeys, ImmutableArray<int> InspectionCounts);
 
 public static class D11P1
 {
-    public static object Part1Answer(this string input) =>
-        new NotImplementedException();
-
-    internal static IEnumerable<Thing> ParseThings(this string input) =>
+    public static int Part1Answer(this string input) =>
         input
-            .Lines()
-            .Select(TryParseAsThing)
-            .OfType<Thing>();
+            .ParseMonkeys()
+            .ToList()
+            .CreateGame()
+            .PlayRounds(20)
+            .GetLevelOfMonkeyBusiness();
 
-    internal static Thing? TryParseAsThing(this string line)
+    internal static IEnumerable<Monkey> ParseMonkeys(this string input) =>
+        input
+            .TrimmedLines()
+            .Scan(default(Monkey), ParseInputLine)
+            .OfType<Monkey>();
+
+    private static (Monkey? RetVal, Monkey? WipMonkey) ParseInputLine(Monkey? wipMonkey, string line, int _)
     {
-        return null;
+        if (string.IsNullOrEmpty(line))
+            return (wipMonkey, null);
+        var split = line.Split(' ');
+        if (split[0].Equals("Monkey")) return StartNewMonkey(wipMonkey, split[1]);
+        if (wipMonkey is null) return (null, wipMonkey);
+        return (split[0], split[1]) switch
+        {
+            ("Starting", _) => AddStartingItems(wipMonkey, split[2..]),
+            ("Operation:", _) => AddOperation(wipMonkey, split[1..]),
+            ("Test:", _) => AddTest(wipMonkey, split[1..]),
+            ("If", "true:") => AddTrueMonkey(wipMonkey, split[1..]),
+            ("If", "false:") => AddFalseMonkey(wipMonkey, split[1..]),
+            _ => throw new InvalidOperationException(split[0])
+        };
     }
 
-    internal static int GetResult(this IEnumerable<Thing> things) => things.Select(AsResult).Sum();
-    internal static int AsResult(this Thing thing) => 0;
+    private static (Monkey? RetVal, Monkey WipMonkey) AddStartingItems(Monkey wipMonkey, string[] rest) =>
+        (null, wipMonkey with
+        {
+            Items = rest
+                .Select(withComma => withComma.Replace(",", ""))
+                .Select(int.Parse)
+                .Aggregate(ImmutableQueue<int>.Empty, (queue, item) => queue.Enqueue(item))
+        });
+
+    private static (Monkey? RetVal, Monkey WipMonkey) AddOperation(Monkey wipMonkey, string[] rest) =>
+        (rest[3], rest[4]) switch
+        {
+            ("*", "old") => (null, wipMonkey with {Operation = old => old * old}),
+            ("+", "old") => (null, wipMonkey with {Operation = old => old + old}),
+            ("*", _) => (null, wipMonkey with {Operation = old => old * int.Parse(rest[4])}),
+            ("+", _) => (null, wipMonkey with {Operation = old => old + int.Parse(rest[4])}),
+            _ => throw new InvalidOperationException()
+        };
+
+    private static (Monkey? RetVal, Monkey WipMonkey) AddTest(Monkey wipMonkey, string[] rest)
+        => (null, wipMonkey with {Test = worry => worry % int.Parse(rest[2]) == 0});
+
+    private static (Monkey? RetVal, Monkey WipMonkey) AddTrueMonkey(Monkey wipMonkey, string[] rest)
+        => (null, wipMonkey with {TrueMonkey = int.Parse(rest[4])});
+
+    private static (Monkey? RetVal, Monkey WipMonkey) AddFalseMonkey(Monkey wipMonkey, string[] rest)
+        => (null, wipMonkey with {FalseMonkey = int.Parse(rest[4])});
+
+    private static (Monkey? RetVal, Monkey WipMonkey) StartNewMonkey(Monkey? wipMonkey, string numberColon)
+    {
+        var monkeyNo = int.Parse(numberColon[..^1]);
+        return (
+            wipMonkey,
+            new Monkey(monkeyNo, ImmutableQueue<int>.Empty, _ => 0, _ => false, -1, -1)
+        );
+    }
+
+    internal static Game CreateGame(this ICollection<Monkey> monkeys) =>
+        new(monkeys.ToImmutableArray(), monkeys.Select(_ => 0).ToImmutableArray());
+
+    internal static Game PlayRounds(this Game game, int count)
+        => Enumerable.Range(0, count).Aggregate(game, PlayRound);
+
+    internal static Game PlayRound(this Game game, int roundNumber)
+        => game.Monkeys.Select(m => m.Index).Aggregate(game, PlayTurn);
+
+    private static Game PlayTurn(Game game, int monkeyIndex) =>
+        game.Monkeys[monkeyIndex].Items
+            .Select(item => (Item: item, MonkeyIndex: monkeyIndex))
+            .Aggregate(game.UpdateMonkey(game.Monkeys[monkeyIndex] with {Items = ImmutableQueue<int>.Empty}),
+                (gm, itemPair) => HandleItem(itemPair.Item, gm.Monkeys[itemPair.MonkeyIndex], gm));
+
+    private static Game HandleItem(int item, Monkey monkey, Game game)
+    {
+        var newItem = monkey.Operation(item) / 3;
+        var targetMonkey = game.Monkeys[monkey.Test(newItem) ? monkey.TrueMonkey : monkey.FalseMonkey];
+        return game
+            .IncreaseInspectionCount(monkey)
+            .UpdateMonkey(targetMonkey with {Items = targetMonkey.Items.Enqueue(newItem)});
+    }
+
+    private static Game UpdateMonkey(this Game game, Monkey monkey)
+        => game with
+        {
+            Monkeys = game.Monkeys.SetItem(monkey.Index, monkey)
+        };
+
+    private static Game IncreaseInspectionCount(this Game game, Monkey monkey)
+        => game with
+        {
+            InspectionCounts = game.InspectionCounts.SetItem(monkey.Index, game.InspectionCounts[monkey.Index] + 1)
+        };
+
+    internal static int GetLevelOfMonkeyBusiness(this Game game)
+        => game
+            .InspectionCounts
+            .OrderByDescending(_ => _)
+            .Take(2)
+            .Multiplied();
+
+    internal static int Multiplied(this IEnumerable<int> numbers)
+        => numbers.Aggregate(1, (prev, num) => prev * num);
 }
--- day11/D11P1Tests.cs
+++ day11/D11P1Tests.cs
@@ -5,34 +5,93 @@ namespace day11;
 
 public static class D11P1Tests
 {
-    [InlineData("",null)]
-    [Theory]
-    internal static void ParseInputLineTest(string line, Thing? expectedThing)
-    {
-        var actualThing = line.TryParseAsThing();
-        actualThing.Should().Be(expectedThing);
-    }
-
     [Fact]
     internal static void ParseInputTest()
     {
-        var things = Input.ExampleInput.ParseThings().ToArray();
-        things.Should().HaveCount(0);
+        var things = Input.ExampleInput.ParseMonkeys().ToArray();
+        things.Should().HaveCount(4);
+
+        var monkey0 = things[0];
+        monkey0.Items.Should().HaveCount(2);
+        monkey0.Items.Peek().Should().Be(79);
+        monkey0.Operation(1).Should().Be(19);
+        monkey0.Test(46).Should().BeTrue();
+        monkey0.Test(47).Should().BeFalse();
+        monkey0.TrueMonkey.Should().Be(2);
+        monkey0.FalseMonkey.Should().Be(3);
+
+        things[1].Operation(1).Should().Be(7);
+        things[2].Operation(4).Should().Be(16);
+        things[2].Operation(5).Should().Be(25);
+
+        var monkey3 = things[3];
+        monkey3.Items.Should().HaveCount(1);
+        monkey3.Items.Peek().Should().Be(74);
+        monkey3.Operation(1).Should().Be(4);
+        monkey3.Test(51).Should().BeTrue();
+        monkey3.Test(47).Should().BeFalse();
+        monkey3.TrueMonkey.Should().Be(0);
+        monkey3.FalseMonkey.Should().Be(1);
     }
 
-    [Fact(Skip="ToDo")]
+    [Fact]
+    internal static void ParseInputSmokeTest()
+    {
+        var things = Input.RawInput.ParseMonkeys().ToArray();
+        things.Should().HaveCount(8);
+
+        foreach (var monkey in things)
+        {
+            monkey.Operation(1);
+            monkey.Test(46);
+        }
+    }
+
+    [Fact]
+    internal static void TestRounds()
+    {
+        var gameAfter1Round = Input.ExampleInput
+            .ParseMonkeys()
+            .ToList()
+            .CreateGame()
+            .PlayRounds(1);
+
+        gameAfter1Round.Monkeys[0].Items.Should().BeEquivalentTo(new[] {20, 23, 27, 26});
+        gameAfter1Round.Monkeys[1].Items.Should().HaveCount(6);
+        gameAfter1Round.Monkeys[2].Items.Should().HaveCount(0);
+        gameAfter1Round.Monkeys[3].Items.Should().HaveCount(0);
+
+        var gameAfter2Rounds = gameAfter1Round.PlayRounds(1);
+        gameAfter2Rounds.Monkeys[0].Items.Should().BeEquivalentTo(new[] { 695, 10,71,135,350 });
+        gameAfter2Rounds.Monkeys[1].Items.Should().HaveCount(5);
+        gameAfter2Rounds.Monkeys[2].Items.Should().HaveCount(0);
+        gameAfter2Rounds.Monkeys[3].Items.Should().HaveCount(0);
+
+        var gameAfter20Rounds = gameAfter2Rounds.PlayRounds(18);
+        gameAfter20Rounds.Monkeys[0].Items.Should().BeEquivalentTo(new[] { 10,12,14,26,34 });
+        gameAfter20Rounds.Monkeys[1].Items.Should().HaveCount(5);
+        gameAfter20Rounds.Monkeys[2].Items.Should().HaveCount(0);
+        gameAfter20Rounds.Monkeys[3].Items.Should().HaveCount(0);
+
+        gameAfter20Rounds.InspectionCounts[0].Should().Be(101);
+        gameAfter20Rounds.InspectionCounts[1].Should().Be(95);
+        gameAfter20Rounds.InspectionCounts[2].Should().Be(7);
+        gameAfter20Rounds.InspectionCounts[3].Should().Be(105);
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 10605;
         Input.ExampleInput
             .Part1Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 50172;
         Input.RawInput
             .Part1Answer()
             .Should().Be(expected);
