79ffffa4b3e2ec66a644ccbdb3fbc238b45db5b4
Bart de Boer <bart.de.boer@divverence.com>
2022-12-13 14:44:29 +0100

    Finish Day 13 Part 1

--- day13/D13P1.cs
+++ day13/D13P1.cs
@@ -1,25 +1,116 @@
-﻿using shared;
+﻿using System.Diagnostics;
+using shared;
 
 namespace day13;
 
-internal record Thing(bool Data);
+internal record Thing
+{
+    internal Thing(int value) => Value = value;
+    internal Thing(Thing[] value) => Things = value;
+    public int? Value { get; init; }
+    public Thing[]? Things { get; init; }
+}
 
 public static class D13P1
 {
-    public static object Part1Answer(this string input) =>
-        new NotImplementedException();
+    public static int Part1Answer(this string input) =>
+        input
+            .ParseThings()
+            .Buffer(2)
+            .Select((pair, index) => (Comparison: pair[0].CompareTo(pair[1]), Index: index + 1))
+            .Where(t => t.Comparison == -1)
+            .Sum(t => t.Index);
 
     internal static IEnumerable<Thing> ParseThings(this string input) =>
         input
-            .Lines()
-            .Select(TryParseAsThing)
-            .OfType<Thing>();
+            .NotEmptyTrimmedLines()
+            .Select(TryParseAsThing);
 
-    internal static Thing? TryParseAsThing(this string line)
+    internal static Thing TryParseAsThing(this string line)
     {
-        return null;
+        Stack<List<Thing>> parents = new();
+        int? valueInWording = null;
+
+        foreach (var ch in line)
+        {
+            switch (ch)
+            {
+                case '[':
+                  var newArray = new List<Thing>();
+                  parents.Push(newArray);
+                  break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    valueInWording = (valueInWording ?? 0) * 10 + (ch - '0');
+                    break;
+
+                case ',':
+                    if (valueInWording.HasValue)
+                        parents.Peek().Add(new Thing(valueInWording.Value));
+                    valueInWording = null;
+                    break;
+
+                case ']':
+                    if (valueInWording.HasValue)
+                        parents.Peek().Add(new Thing(valueInWording.Value));
+                    valueInWording = null;
+
+                    var theArray = parents.Pop().ToArray();
+                    var newThing = new Thing(theArray);
+                    if (parents.Count == 0)
+                        return newThing;
+                    parents.Peek().Add(newThing);
+                    break;
+            }
+        }
+
+        if (valueInWording.HasValue)
+            return new(valueInWording.Value);
+
+        throw new UnreachableException();
     }
 
-    internal static int GetResult(this IEnumerable<Thing> things) => things.Select(AsResult).Sum();
-    internal static int AsResult(this Thing thing) => 0;
+    internal static int CompareTo(this Thing lhs, Thing rhs)
+    {
+        return (lhs.Value, lhs.Things, rhs.Value, rhs.Things) switch
+        {
+            ({ } l, null, { } r, null) => l.CompareTo(r),
+            ({ } l, null, null, { }) => l.Wrapped().CompareTo(rhs),
+            (null, { } l, null, { } r) => l.CompareTo(r),
+            (null, { }, { } r, null) => lhs.CompareTo(r.Wrapped()),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    internal static int CompareTo(this Thing[] lhs, Thing[] rhs)
+    {
+        var commonLength = Math.Min(lhs.Length, rhs.Length);
+        var result = lhs.Take(commonLength).Zip(rhs.Take(commonLength))
+            .Select(pair => (int?)pair.First.CompareTo(pair.Second))
+            .FirstOrDefault(r => r != 0);
+        
+        return result ?? lhs.Length.CompareTo(rhs.Length);
+    }
+
+    internal static Thing Wrapped(this int value) => new(new Thing[] {new(value)});
+
+    internal static string Render(this Thing thing) => System.Text.Json.JsonSerializer.Serialize(thing.ToObject());
+
+    internal static object ToObject(this Thing thing) =>
+
+        thing switch
+        {
+            {Things: { }} => thing.Things.Select(ToObject),
+            {Value: { } v} => v,
+            _ => throw new UnreachableException()
+        };
 }
--- day13/D13P1Tests.cs
+++ day13/D13P1Tests.cs
@@ -1,38 +1,68 @@
 ﻿using FluentAssertions;
+using shared;
 using Xunit;
 
 namespace day13;
 
 public static class D13P1Tests
 {
-    [InlineData("",null)]
-    [Theory]
-    internal static void ParseInputLineTest(string line, Thing? expectedThing)
+    [Fact]
+    internal static void ParseInputLineTest()
     {
-        var actualThing = line.TryParseAsThing();
-        actualThing.Should().Be(expectedThing);
+        "[]".TryParseAsThing().Things.Should().BeEquivalentTo(Array.Empty<Thing>());
+        "[9]".TryParseAsThing().Things.Should().BeEquivalentTo(new Thing[] {new(9)});
+        "[7,7,7]".TryParseAsThing().Things.Should().BeEquivalentTo(new Thing[] {new(7), new(7) , new(7) });
+        "[[1],4]".TryParseAsThing().Things.Should().BeEquivalentTo(new Thing[] {new(new Thing[]{new(1)}), new(4)});
+        "9".TryParseAsThing().Should().BeEquivalentTo(new Thing(9));
+        "0".TryParseAsThing().Should().BeEquivalentTo(new Thing(0));
     }
 
     [Fact]
     internal static void ParseInputTest()
     {
         var things = Input.ExampleInput.ParseThings().ToArray();
-        things.Should().HaveCount(0);
+        things.Should().HaveCount(16);
     }
 
-    [Fact(Skip="ToDo")]
+    [Fact]
+    internal static void RoundtripTest()
+    {
+        foreach (var line in Input.ExampleInput
+                     .NotEmptyTrimmedLines())
+            line.TryParseAsThing().Render().Should().Be(line);
+        foreach (var line in Input.RawInput
+                     .NotEmptyTrimmedLines()) 
+            line.TryParseAsThing().Render().Should().Be(line);
+    }
+
+    [Fact]
+    internal static void CompareTest()
+    {
+        "9".TryParseAsThing().CompareTo("9".TryParseAsThing()).Should().Be(0);
+        "[9]".TryParseAsThing().CompareTo("[9]".TryParseAsThing()).Should().Be(0);
+        "[1,1,3,1,1]".TryParseAsThing().CompareTo("[1,1,5,1,1]".TryParseAsThing()).Should().Be(-1);
+        "[[1],[2,3,4]]".TryParseAsThing().CompareTo("[[1],4]".TryParseAsThing()).Should().Be(-1);
+        "[9]".TryParseAsThing().CompareTo("[[8,7,6]]".TryParseAsThing()).Should().Be(1);
+        "[[4,4],4,4]".TryParseAsThing().CompareTo("[[4,4],4,4,4]".TryParseAsThing()).Should().Be(-1);
+        "[7,7,7,7]".TryParseAsThing().CompareTo("[7,7,7]".TryParseAsThing()).Should().Be(1);
+        "[]".TryParseAsThing().CompareTo("[3]".TryParseAsThing()).Should().Be(-1);
+        "[[[]]]".TryParseAsThing().CompareTo("[[]]".TryParseAsThing()).Should().Be(1);
+        "[1,[2,[3,[4,[5,6,7]]]],8,9]".TryParseAsThing().CompareTo("[1,[2,[3,[4,[5,6,0]]]],8,9]".TryParseAsThing()).Should().Be(1);
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 13;
         Input.ExampleInput
             .Part1Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 6369;
         Input.RawInput
             .Part1Answer()
             .Should().Be(expected);
