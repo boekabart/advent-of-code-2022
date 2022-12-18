843d54c918bbee253726bab91225cb7483a67b99
Bart de Boer <bart.deboer@tele2.com>
2022-12-18 23:34:09 +0100

    Fix clone.ps1 on mac

--- day__/clone.ps1
+++ day__/clone.ps1
@@ -1,11 +1,10 @@
 if ($args.count -eq 0) { write-host "Pass number please"; return }
 $X=$args[0]
-rd "..\day$X" -force -recurse
-mkdir "..\day$X"
-copy *.cs* "..\day$X"
-cd "..\day$X"
-fart -f *.* __ $X
-fart *.cs __ $x
-fart *.sln day__ "day$X"
+Remove-Item "../day$X" -force -recurse
+mkdir "../day$X"
+Copy-Item *.cs* "../day$X"
+Set-Location "../day$X"
+fart -f "*.*" __ $X
+fart "*.cs" __ $x
 dotnet build
-cd ..\day__
+Set-Location "../day__"
